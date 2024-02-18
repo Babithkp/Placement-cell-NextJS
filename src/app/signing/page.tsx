c5cfaed40fb449e26bedbaeddabef16cf09d3b3e
Babithkp <babithkullachetti44@gmail.com>
2024-02-18 10:19:11 +0530

    removed api key

--- src/app/signing/page.tsx
+++ src/app/signing/page.tsx
@@ -1,14 +1,5 @@
-"use client"
 import CardContainer from "@/components/login/CardContainer";
-import React from "react";
-
-
-
-
-
 export default function Page() {
-
-
   return (
     <div className="flex items-center justify-center rounded-3xl drop-shadow-2xl" >
       <CardContainer/>
