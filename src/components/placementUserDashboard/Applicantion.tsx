d324e089e34353099f14b4d81ce0d2fc20fe5f4f
Babithkp <babithkullachetti44@gmail.com>
2024-03-10 07:47:01 +0530

    bug fix

--- src/components/placementUserDashboard/Applicantion.tsx
+++ src/components/placementUserDashboard/Applicantion.tsx
@@ -2,12 +2,14 @@
 import React, { useEffect, useState } from "react";
 import StudentsDetails from "./StudentsDetails";
 
-export default function Applicantion({ jobList, reFetch }: any) {
+export default function Applicantion({ jobList,reFetch}: any) {
   const [jobDetails, setJobDetails] = useState<any[]>([]);
+
+
 
   useEffect(() => {
     async function fetch() {
-      setJobDetails(jobList);
+      setJobDetails(jobList);      
     }
     fetch();
   }, [jobList]);
@@ -16,22 +18,22 @@ export default function Applicantion({ jobList, reFetch }: any) {
     <div className="w-[90%] ">
       {jobDetails?.map((job) => (
         <div key={job._id}>
-          <div>
-            <h4 className="text-medium rounded-t-lg bg-[#2560a9] py-1 pl-10 text-white">
-              {job.jobtTitle}
-            </h4>
+          {job.applicants.length > 0 ? (
             <div>
-              {job.applicants.map((singStud: any, i: string) => (
-                <div key={i}>
-                  <StudentsDetails
-                    students={singStud}
-                    jobId={job._id}
-                    reFetch={reFetch}
-                  />
-                </div>
-              ))}
+              <h4 className="text-medium rounded-t-lg bg-[#2560a9] py-1 pl-10 text-white">
+                {job.jobtTitle}
+              </h4>
+              <div>
+                {job.applicants.map((singStud:any,i:string)=>(
+                  <div key={i}>
+                    <StudentsDetails students={ singStud} jobId={job._id} reFetch={reFetch}/>
+                  </div>
+                  ))}
+              </div>
             </div>
-          </div>
+          ) : (
+            ""
+          )}
         </div>
       ))}
     </div>
--- src/components/placementUserDashboard/SelectedList.tsx
+++ src/components/placementUserDashboard/SelectedList.tsx
@@ -2,32 +2,31 @@
 import React, { useEffect, useState } from "react";
 import StudentsDetails from "./StudentsDetails";
 
-export default function SelectedList({ jobList, reFetch }: any) {
+export default function SelectedList({ jobList,reFetch}: any) {
   const [jobDetails, setJobDetails] = useState<any[]>([]);
 
   useEffect(() => {
-    setJobDetails(jobList);
+      setJobDetails(jobList);      
   }, [jobList]);
 
   return (
     <div className="w-[90%] ">
       {jobDetails?.map((job) => (
         <div key={job._id}>
-          <div>
-            <h4 className="text-medium rounded-t-lg bg-[#2560a9] py-1 pl-10 text-white">
-              {job.jobtTitle}
-            </h4>
-            {job.selectApplicants.map((student: any, i: string) => (
+          {job.selectApplicants.length > 0 ? (
+            <div>
+              <h4 className="text-medium rounded-t-lg bg-[#2560a9] py-1 pl-10 text-white">
+                {job.jobtTitle}
+              </h4>
+              {job.selectApplicants.map((student:any,i:string)=>(
               <div key={i}>
-                <StudentsDetails
-                  students={student}
-                  selected={true}
-                  jobId={job._id}
-                  reFetch={reFetch}
-                />
+                <StudentsDetails students={student} selected={true} jobId={job._id} reFetch={reFetch}/>
               </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          ) : (
+            ""
+          )}
         </div>
       ))}
     </div>
